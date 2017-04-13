c8eb99ed7e291183678afec09583a39c6efa89a3
Mr.J <slashchenxiaojun@sina.com>
2017-04-13 16:31:22 +0800

    关于java的方法调用的学习

---
+++ src/test/java/org/hacker/diary/java/base/MethodInvoke.java
@@ -0,0 +1,22 @@
+package org.hacker.diary.java.base;
+
+public class MethodInvoke {
+  static abstract class Human {}
+  static class Man extends Human {}
+  static class Woman extends Human {}
+  public static void sayHello(Human human) {
+    System.out.println("hello human!");
+  }
+  public static void sayHello(Man man) {
+    System.out.println("hello man!");
+  }
+  public static void sayHello(Woman woman) {
+    System.out.println("hello woman!");
+  }
+  public static void main(String[] args) {
+    Human man = new Man();
+    Human woman = new Woman();
+    MethodInvoke.sayHello(man);
+    MethodInvoke.sayHello(woman);
+  }
+}
